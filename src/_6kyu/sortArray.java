a027cecf05a7a2d22a8f6c5464b017a38df6c917
tolga güreli <tolgagureli@outlook.com>
2022-04-07 23:20:03 +0300

    _6kyu sortArray solution

---
+++ src/_6kyu/sortArray.java
@@ -0,0 +1,36 @@
+package _6kyu;
+
+import java.util.Arrays;
+
+public class sortArray {
+
+    public static void main(String[] args) {
+        System.out.println(sortArray(new int[]{ 5, 3, 1, 8, 0 }));
+    }
+    public static int[] sortArray(int[] array) {
+        int number = Integer.MAX_VALUE;
+        int[] ints = new int[array.length];
+
+        for (int i = 0; i < array.length-1; i++) {
+
+            for (int j = 0; j < array.length; j++) {
+                if (array[j]%2==1 &&array[i]%2==1 && array[j]>array[i]){
+                    number = array[i];
+                    array[i] = array[j];
+                    array[j] = number;
+                }
+            }
+        }
+/*
+    // Sort the odd numbers only
+    final int[] sortedOdd = Arrays.stream(array).filter(e -> e % 2 == 1).sorted().toArray();
+
+    // Then merge them back into original array
+    for (int j = 0, s = 0; j < array.length; j++) {
+      if (array[j] % 2 == 1) array[j] = sortedOdd[s++];
+    }
+*/
+        return Arrays.stream(array).toArray();
+
+    }
+}
