ddaadee35fc528a4c79e84f1214869e787303483
tolga güreli <tolgagureli@outlook.com>
2022-04-07 22:32:07 +0300

    _6kyu CountingDuplicates solution

---
+++ src/_6kyu/CountingDuplicates.java
@@ -0,0 +1,52 @@
+package _6kyu;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+public class CountingDuplicates {
+    public static int duplicateCount(String text) {
+        return (int) Arrays.stream(text.toLowerCase().split(""))
+                .collect(Collectors.groupingBy(s -> s,Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(integerListEntry -> integerListEntry.getValue()>0)
+                .map(stringLongEntry -> stringLongEntry.getKey())
+                .count();
+
+    }
+
+    @Test
+    public void abcdeReturnsZero() {
+        assertEquals(0, CountingDuplicates.duplicateCount("abcde"));
+    }
+
+    @Test
+    public void abcdeaReturnsOne() {
+        assertEquals(1, CountingDuplicates.duplicateCount("abcdea"));
+    }
+
+    @Test
+    public void indivisibilityReturnsOne() {
+        assertEquals(1, CountingDuplicates.duplicateCount("indivisibility"));
+    }
+
+    @Test
+    public void reallyLongStringContainingDuplicatesReturnsThree() {
+        String testThousandA = new String(new char[1000]).replace('\0', 'a');
+        String testHundredB = new String(new char[100]).replace('\0', 'b');
+        String testTenC = new String(new char[10]).replace('\0', 'c');
+        String test1CapitalA = new String(new char[1]).replace('\0', 'A');
+        String test1d = new String(new char[1]).replace('\0', 'd');
+        String test = test1d + test1CapitalA + testTenC +
+                testHundredB + testThousandA;
+
+
+        assertEquals(3, CountingDuplicates.duplicateCount(test));
+    }
+
+}
